8271d42f6cdf4cb5ac28abbf73831e67ed73eb02
kawax <kawaxbiz@gmail.com>
2025-06-17 13:50:22 +0900

    Update ArtisanFeed.php

--- workbench/app/FeedGenerator/ArtisanFeed.php
+++ workbench/app/FeedGenerator/ArtisanFeed.php
@@ -12,7 +12,12 @@ class ArtisanFeed implements FeedGeneratorAlgorithm
 {
     public function __invoke(int $limit, ?string $cursor, ?string $user, Request $request): array
     {
+        // Use searchPosts() in the public api. It cannot be used when restricted, so use the authentication method below.
         $response = Bluesky::public()->searchPosts(q: '#laravel', until: $cursor, limit: $limit);
+
+        // Using searchPosts() with authentication
+        $response = Bluesky::login(identifier: config('bluesky.identifier'), password: config('bluesky.password'))
+                        ->searchPosts(q: $q, limit: 10);
 
         $cursor = data_get($response->collect('posts')->last(), 'indexedAt', '');
 
